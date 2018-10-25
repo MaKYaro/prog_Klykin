c079991744e8769511d463609ecdc95cc2e38d86
MaKYaro <43673042+MaKYaro@users.noreply.github.com>
2018-10-25 23:43:17 +0300

    Ввел ограничения для чисел больше 24 и 2

--- robot.my/task6.py
+++ robot.my/task6.py
@@ -5,43 +5,44 @@ r.lm('task6')
 def task():
     x = int(input("Enter letter width: "))
     y = int(input("Enter letter height: "))
-    if x-2*(x//2)<1:
-        r.paint()
-        r.down()
-        r.paint()
-        for w in range(x//2-1):
-            r.right()
+    if x > 24 or y > 24 or x == 2:
+        print("Error")
+    else:
+        if x-2*(x//2)<1:
             r.paint()
-        for h in range(y-2):
             r.down()
             r.paint()
-        r.right()
-        r.paint()
-        for h in range(y-2):
+            for w in range(x//2-1):
+                r.right()
+                r.paint()
+            for h in range(y-2):
+                r.down()
+                r.paint()
+            r.right()
+            r.paint()
+            for h in range(y-2):
+                r.up()
+                r.paint()
+            for w in range(x//2-1):
+                r.right()
+                r.paint()
             r.up()
             r.paint()
-        for w in range(x//2-1):
-            r.right()
+            for x in range(x-1):
+                r.left()
+                r.paint()
+        else:
+            for w in range(x//2):
+                r.paint()
+                r.right()
+            for h in range(y-1):
+                r.down()
+                r.paint()
+            r.up(y-1)
             r.paint()
-        r.up()
-        r.paint()
-        for x in range(x-1):
-            r.left()
-            r.paint()
-    else:
-        for w in range(x//2):
-            r.paint()
-            r.right()
-        for h in range(y-1):
-            r.down()
-            r.paint()
-        r.up(y-1)
-        r.paint()
-        for w in range(x//2):
-            r.right()
-            r.paint()
-
-
-
-
+            for w in range(x//2):
+                r.right()
+                r.paint()
+                
+                
 r.start(task)
