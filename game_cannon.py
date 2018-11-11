7c8054a1f70c965a1544b0880c0808582c6e7f3c
MaKYaro <klykin-2@yandex.ru>
2018-11-12 00:26:25 +0300

    Скорость ядра теперь зависит от времени удержания левой клавиши мыши

--- game_cannon.py
+++ game_cannon.py
@@ -1,12 +1,14 @@
 import math
 from tkinter import *
+import time
 
-
+time_counter = 0
 g = 9.8  # Ускорение свободного падения для снаряда.
 
 
 class Cannon:
     max_velocity = 100
+
 
     def __init__(self, x, y, canvas):
         self.canvas = canvas
@@ -14,10 +16,13 @@ class Cannon:
         self.y = y
         self.shell_num = None  # TODO: оставшееся на данный момент количество снарядов
         self.direction = math.pi/4
+        self.power = 2
+        self.power_speed = 0
 
         self.line_length = 100
         self.line = canv.create_line(x + 70, y - 50, 160, 480, width=30, fill="black")
         self.oval = canv.create_oval(x, y, 100, 500, outline="black", fill="black")
+
 
     def aim(self, x, y):
         """
@@ -32,7 +37,8 @@ class Cannon:
 
         self.draw()
 
-    def fire(self, dt):
+
+    def fire(self):
         """
         Создаёт объект снаряда (если ещё не потрачены все снаряды)
         летящий в направлении угла direction
@@ -40,15 +46,22 @@ class Cannon:
         :param dt:  длительность клика мышки, мс
         :return: экземпляр снаряда типа Shell
         """
-
+        print(self.stop_time)
+        print(self.start_time)
+        self.time_length = self.stop_time - self.start_time
+        self.power_speed = (self.power * self.time_length)/3
         shell = Shell(self.x + 70 + self.line_length*math.cos(self.direction),
                       self.y - 50 + self.line_length*math.sin(self.direction),
-                      self.max_velocity/2, self.max_velocity/2, self.canvas, self.direction)
+                      self.power_speed, self.power_speed, self.canvas, self.direction)
 
         shells.append(shell)
+        print(self.time_length)
 
     def draw(self):
-
+        """
+        Рисует дуло пушки, которое движется в зависимости от перемещений мышки
+        :return:
+        """
         self.canvas.delete(self.line)
 
         self.line = self.canvas.create_line(
@@ -57,6 +70,8 @@ class Cannon:
             self.x + 70 + self.line_length*math.cos(self.direction),
             self.y - 50 + self.line_length*math.sin(self.direction), width=30, fill="black"
         )
+
+
 
 
 class Shell:
@@ -86,8 +101,8 @@ class Shell:
         Сдвигает снаряд исходя из его кинематических характеристик
         и длины кванта времени dt
         в новое положение, а также меняет его скорость.
-        :param dt:
-        :return:
+        :param dt: время элементарного перемещения
+        :return: Движущийся снаряд
         """
 
         ax, ay = 0, g
@@ -98,13 +113,16 @@ class Shell:
         self.Vx += ax * dt
         self.Vy += -ay * dt
 
-        print('x = {}, y = {}'.format(self.x, self.y))
         self.draw()
 
         # TODO: Уничтожать (в статус deleted) снаряд, когда он касается земли.
 
 
     def draw(self):
+        """
+        Рисует движущийся снаряд
+        :return:
+        """
         self.canvas.move(self.oval, self.delta_x, self.delta_y)
 
 
@@ -156,18 +174,49 @@ class Target:
 
 
 def mouse_move_handler(event):
+    """
+    Направляет дуло пушки в сторону курсора
+    :param event: перемещение курсора по экрану
+    :return: Координаты курсорв мыши
+    """
     cannon.aim(event.x, event.y)
 
 
-def mouse_left_click_handler(event):
-    cannon.fire(100)
-
-
 def tick():
+    """
+    Считает время нажатия клавиши мыши
+    :return:
+    """
+    global time_counter
+    time_counter += 1
     for shell in shells:
         shell.go(0.1)
 
     root.after(10, tick)
+
+
+def time_start(event):
+    """
+    Включает счетчик в момент нажания левой клавиши мыши
+    :param event: Момент нажатия левой клавиши
+    :return: Начальное значение времени
+    """
+    global time_counter
+    cannon.start_time = time_counter
+
+
+def time_stop(event):
+    """
+    Выключает счетчик после отпускания левой клавиши
+    Обнуляет счетчик
+    Запускает снаряд в момент момент отпускания левой клавиши
+    :param event: Момент отпускания левой клавиши
+    :return: Конечное значение времени
+    """
+    global time_counter
+    cannon.stop_time = time_counter
+    cannon.fire()
+    time_counter = 0
 
 
 root = Tk()
@@ -177,8 +226,8 @@ canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 
 canv.bind('<Motion>', mouse_move_handler)
-canv.bind('<Button-1>', mouse_left_click_handler)
-
+canv.bind("<ButtonPress-1>", time_start)
+canv.bind("<ButtonRelease-1>", time_stop)
 shells = []
 
 cannon = Cannon(70, 550, canv)
