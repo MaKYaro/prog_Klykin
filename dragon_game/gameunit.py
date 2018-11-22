d55ab4acdc139ee1454f28f63f1ce55b55c0c3da
MaKYaro <klykin-2@yandex.ru>
2018-11-22 22:34:00 +0300

    Создан общий предковый класс для всех объектов игры

---
+++ dragon_game/gameunit.py
@@ -0,0 +1,21 @@
+class Attacker:
+    """Общий класс для всех объектов игры"""
+    _health = None
+    _attack = None
+    _answer = None
+
+    def attack(self, target):
+        """
+        Нападение на соперника
+        :param target: соперник
+        :return: Value
+        """
+        target._health -= self._attack
+
+    def is_alive(self):
+        """
+        Проверка health
+        :return: Value
+        """
+
+        return self._health > 0
