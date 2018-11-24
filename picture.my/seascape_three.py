9e1f8b58f614f103325d8f50435e369041d00204
MaKYaro <klykin-2@yandex.ru>
2018-11-25 00:55:01 +0300

    Убрал copy paste, сделал циклы.

--- picture.my/seascape_three.py
+++ picture.my/seascape_three.py
@@ -18,141 +18,67 @@ def dr_sea():
 
 
 def dr_wave():
-    wave1 = gr.Line(gr.Point(50, 400), gr.Point(300, 400))
-    wave1.setOutline('white')
-    wave1.draw(window)
+    line_coords_list = [gr.Point(50, 400), gr.Point(300, 400),
+                        gr.Point(100, 450), gr.Point(150, 450),
+                        gr.Point(300, 300), gr.Point(400, 300),
+                        gr.Point(10, 350), gr.Point(210, 350),
+                        gr.Point(10, 300), gr.Point(110, 300),
+                        gr.Point(200, 450), gr.Point(400, 450),
+                        gr.Point(300, 425), gr.Point(425, 425),
+                        gr.Point(200, 375), gr.Point(450, 375)]
 
-    wave2 = gr.Line(gr.Point(100, 450), gr.Point(150, 450))
-    wave2.setOutline('white')
-    wave2.draw(window)
+    while len(line_coords_list) != 0:
 
-    wave3 = gr.Line(gr.Point(300, 300), gr.Point(400, 300))
-    wave3.setOutline('white')
-    wave3.draw(window)
-
-    wave4 = gr.Line(gr.Point(10, 350), gr.Point(210, 350))
-    wave4.setOutline('white')
-    wave4.draw(window)
-
-    wave5 = gr.Line(gr.Point(10, 300), gr.Point(110, 300))
-    wave5.setOutline('white')
-    wave5.draw(window)
-
-    wave6 = gr.Line(gr.Point(200, 450), gr.Point(400, 450))
-    wave6.setOutline('white')
-    wave6.draw(window)
-
-    wave7 = gr.Line(gr.Point(300, 425), gr.Point(425, 425))
-    wave7.setOutline('white')
-    wave7.draw(window)
-
-    wave8 = gr.Line(gr.Point(200, 375), gr.Point(450, 375))
-    wave8.setOutline('white')
-    wave8.draw(window)
+        wave = gr.Line(line_coords_list.pop(0), line_coords_list.pop(0))
+        wave.setOutline('white')
+        wave.draw(window)
 
 
-def dr_ship_one():
-    ship1 = gr.Line(gr.Point(230, 400), gr.Point(375, 400))
-    ship1.setWidth(18)
-    ship1.setOutline('brown')
-    ship1.draw(window)
+def dr_ship():
+    list_brown_boards_coords = [gr.Point(230, 400), gr.Point(375, 400),
+                                gr.Point(240, 418), gr.Point(365, 418),
+                                gr.Point(250, 436), gr.Point(355, 436)]
+    list_black_lines_coords = [gr.Point(240, 409), gr.Point(365, 409),
+                               gr.Point(240, 427), gr.Point(365, 427)]
+    while len(list_brown_boards_coords) != 0:
+        ship_brown_line = gr.Line(list_brown_boards_coords.pop(0), list_brown_boards_coords.pop(0))
+        ship_brown_line.setWidth(18)
+        ship_brown_line.setOutline('brown')
+        ship_brown_line.draw(window)
 
-    ship2 = gr.Line(gr.Point(240, 418), gr.Point(365, 418))
-    ship2.setWidth(18)
-    ship2.setOutline('brown')
-    ship2.draw(window)
+    while len(list_black_lines_coords) != 0:
+        ship_black_line = gr.Line(list_black_lines_coords.pop(0), list_black_lines_coords.pop(0))
+        ship_black_line.setOutline('black')
+        ship_black_line.draw(window)
 
-    ship3 = gr.Line(gr.Point(240, 409), gr.Point(365, 409))
-    ship3.setOutline('black')
-    ship3.draw(window)
-
-    ship4 = gr.Line(gr.Point(250, 436), gr.Point(355, 436))
-    ship4.setWidth(18)
-    ship4.setOutline('brown')
-    ship4.draw(window)
-
-    ship5 = gr.Line(gr.Point(240, 427), gr.Point(365, 427))
-    ship5.setOutline('black')
-    ship5.draw(window)
-
-    ship6 = gr.Line(gr.Point(300, 400), gr.Point(300, 300))
-    ship6.setWidth(5)
-    ship6.setOutline('brown')
-    ship6.draw(window)
+    mast = gr.Line(gr.Point(300, 400), gr.Point(300, 300))
+    mast.setWidth(5)
+    mast.setOutline('brown')
+    mast.draw(window)
 
     sail = gr.Oval(gr.Point(290, 385), gr.Point(320, 310))
     sail.setFill('white')
     sail.draw(window)
 
 
-def dr_ship_two():
-    ship1 = gr.Line(gr.Point(130, 300), gr.Point(275, 300))
-    ship1.setWidth(18)
-    ship1.setOutline('brown')
-    ship1.draw(window)
-
-    ship2 = gr.Line(gr.Point(140, 318), gr.Point(265, 318))
-    ship2.setWidth(18)
-    ship2.setOutline('brown')
-    ship2.draw(window)
-
-    ship3 = gr.Line(gr.Point(140, 309), gr.Point(265, 309))
-    ship3.setOutline('black')
-    ship3.draw(window)
-
-    ship4 = gr.Line(gr.Point(150, 336), gr.Point(255, 336))
-    ship4.setWidth(18)
-    ship4.setOutline('brown')
-    ship4.draw(window)
-
-    ship5 = gr.Line(gr.Point(140, 327), gr.Point(265, 327))
-    ship5.setOutline('black')
-    ship5.draw(window)
-
-    ship6 = gr.Line(gr.Point(200, 300), gr.Point(200, 200))
-    ship6.setWidth(5)
-    ship6.setOutline('brown')
-    ship6.draw(window)
-
-    sail = gr.Oval(gr.Point(190, 285), gr.Point(220, 210))
-    sail.setFill('white')
-    sail.draw(window)
-
-
 def dr_cloud1():
-    cloud1 = gr.Circle(gr.Point(0, 20), 40)
-    cloud1.setFill('white')
-    cloud1.draw(window)
-
-    cloud2 = gr.Circle(gr.Point(70, 20), 46)
-    cloud2.setFill('white')
-    cloud2.draw(window)
-
-    cloud3 = gr.Circle(gr.Point(10, 60), 22)
-    cloud3.setFill('white')
-    cloud3.draw(window)
-
-    cloud4 = gr.Circle(gr.Point(40, 50), 30)
-    cloud4.setFill('white')
-    cloud4.draw(window)
-
-
-def dr_cloud2():
-    cloud5 = gr.Circle(gr.Point(500, 50), 60)
-    cloud5.setFill('white')
-    cloud5.draw(window)
-
-    cloud6 = gr.Circle(gr.Point(450, 20), 30)
-    cloud6.setFill('white')
-    cloud6.draw(window)
+    list_cloud_coords = [gr.Point(0, 20), 40,
+                         gr.Point(70, 20), 46,
+                         gr.Point(10, 60), 22,
+                         gr.Point(40, 50), 30,
+                         gr.Point(500, 50), 60,
+                         gr.Point(450, 20), 30]
+    while len(list_cloud_coords) != 0:
+        cloud = gr.Circle(list_cloud_coords.pop(0), list_cloud_coords.pop(0))
+        cloud.setFill('white')
+        cloud.draw(window)
 
 
 dr_sky()
 dr_sea()
 dr_wave()
-dr_ship_one()
-dr_ship_two()
-dr_cloud1()
-dr_cloud2()
+dr_ship()
+dr_cloud()
+
 
 window.getMouse()
